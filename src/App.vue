7c245445a0adb65cc9c910ee233a83910783bfb3
wu <1977627369@qq.com>
2020-09-03 22:04:41 +0800

    架子

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-
+    <main-tab-bar></main-tab-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  components: {
-
+  import MainTabBar from "components/content/mainTabBar/MainTabBar";
+  export default {
+    name: 'App',
+    components: {
+      MainTabBar
+    }
   }
-}
 </script>
 
 <style>
-
+  @import "assets/css/base.css";
 </style>
